d49c7888c64336b714103d193c25a1c5c4488b35
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2024-05-31 19:36:00 -0300

    refactor: Update PaymentFacadeFactory create method to return a PaymentFacadeInterface

--- src/modules/payment/factory/payment.facade.factory.ts
+++ src/modules/payment/factory/payment.facade.factory.ts
@@ -1,9 +1,10 @@
 import { PaymentFacade } from '@/modules/payment/facade/payment.facace'
+import { PaymentFacadeInterface } from '@/modules/payment/facade/payment.facade.interface'
 import { TransactionRepository } from '@/modules/payment/repository/transaction.repository'
 import ProcessPaymentUsecase from '@/modules/payment/usecase/process-payment/process-payment.usecase'
 
 export class PaymentFacadeFactory {
-  static create() {
+  static create(): PaymentFacadeInterface {
     const repository = new TransactionRepository()
     const props = {
       processPaymentUsecase: new ProcessPaymentUsecase(repository),
