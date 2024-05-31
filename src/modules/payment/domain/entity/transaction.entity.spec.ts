e4e2977a4109a4e18bfa81485c614498548c38e4
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2024-05-31 14:04:36 -0300

    feat: add transaction

---
+++ src/modules/payment/domain/entity/transaction.entity.spec.ts
@@ -0,0 +1,134 @@
+import { Id } from '@/modules/@shared/domain/value-object/id.value-object'
+import {
+  Transaction,
+  TransactionStatus,
+} from '@/modules/payment/domain/entity/transaction.entity'
+
+describe('Transaction entity unit tests', () => {
+  it('should create a transaction entity with minimum required properties', () => {
+    // Arrange - Given
+    const props = {
+      amount: 100,
+      orderId: new Id(),
+    }
+
+    // Act - When
+    const transaction = new Transaction(props)
+
+    // Assert - Then
+    expect(transaction).toBeInstanceOf(Transaction)
+    expect(transaction.id).toBeInstanceOf(Id)
+    expect(transaction.createdAt).not.toBeNull()
+    expect(transaction.updatedAt).not.toBeNull()
+    expect(transaction.amount).toBe(props.amount)
+    expect(transaction.orderId).toBeInstanceOf(Id)
+    expect(transaction.orderId).toBe(props.orderId)
+    expect(transaction.status).toBe('pending')
+  })
+
+  it('should create a transaction entity with all properties', () => {
+    // Arrange - Given
+    const props = {
+      id: new Id(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      amount: 100,
+      orderId: new Id(),
+      status: TransactionStatus.Approved,
+    }
+
+    // Act - When
+    const transaction = new Transaction(props)
+
+    // Assert - Then
+    expect(transaction).toBeInstanceOf(Transaction)
+    expect(transaction.id).toBe(props.id)
+    expect(transaction.createdAt).toBe(props.createdAt)
+    expect(transaction.updatedAt).toBe(props.updatedAt)
+    expect(transaction.amount).toBe(props.amount)
+    expect(transaction.orderId).toBe(props.orderId)
+    expect(transaction.status).toBe(props.status)
+  })
+
+  it('should throw an error if amount is less than or equal to 0', () => {
+    // Arrange - Given
+    const props = {
+      amount: 0,
+      orderId: new Id(),
+    }
+
+    // Act - When
+    const createTransaction = () => new Transaction(props)
+
+    // Assert - Then
+    expect(createTransaction).toThrow(
+      'payment/transaction: Amount must be greater than 0'
+    )
+  })
+
+  it('should proccess and approve a transaction', () => {
+    // Arrange - Given
+    const props = {
+      amount: 100,
+      orderId: new Id(),
+    }
+    const transaction = new Transaction(props)
+
+    // Act - When
+    transaction.process()
+
+    // Assert - Then
+    expect(transaction.status).toBe(TransactionStatus.Approved)
+  })
+
+  it('should process and decline a transaction', () => {
+    // Arrange - Given
+    const props = {
+      amount: 99,
+      orderId: new Id(),
+    }
+    const transaction = new Transaction(props)
+
+    // Act - When
+    transaction.process()
+
+    // Assert - Then
+    expect(transaction.status).toBe(TransactionStatus.Declined)
+  })
+
+  it('should process a transaction and throw an error if the the transaction is already declined', () => {
+    // Arrange - Given
+    const props = {
+      amount: 100,
+      orderId: new Id(),
+      status: TransactionStatus.Declined,
+    }
+    const transaction = new Transaction(props)
+
+    // Act - When
+    const processTransaction = () => transaction.process()
+
+    // Assert - Then
+    expect(processTransaction).toThrow(
+      'payment/transaction: Transaction declined'
+    )
+  })
+
+  it('should process a transaction and throw an error if the the transaction is already approved', () => {
+    // Arrange - Given
+    const props = {
+      amount: 100,
+      orderId: new Id(),
+      status: TransactionStatus.Approved,
+    }
+    const transaction = new Transaction(props)
+
+    // Act - When
+    const processTransaction = () => transaction.process()
+
+    // Assert - Then
+    expect(processTransaction).toThrow(
+      'payment/transaction: Transaction already approved'
+    )
+  })
+})
---
+++ src/modules/payment/domain/entity/transaction.entity.ts
@@ -0,0 +1,82 @@
+import { AggregateRoot } from '@/modules/@shared/domain/entity/aggregate-root.interface'
+import { BaseEntity } from '@/modules/@shared/domain/entity/base.entity'
+import { Id } from '@/modules/@shared/domain/value-object/id.value-object'
+
+export enum TransactionStatus {
+  Approved = 'approved',
+  Declined = 'declined',
+  Pending = 'pending',
+}
+
+type TransactionProps = {
+  id?: Id
+  createdAt?: Date
+  updatedAt?: Date
+  amount: number
+  orderId: Id
+  status?: TransactionStatus
+}
+
+export class Transaction extends BaseEntity implements AggregateRoot {
+  private _amount: number
+  private _orderId: Id
+  private _status: TransactionStatus
+
+  constructor(props: TransactionProps) {
+    super(props.id, props.createdAt, props.updatedAt)
+    this._amount = props.amount
+    this._orderId = props.orderId
+    this._status = props.status || TransactionStatus.Pending
+    this.validate()
+  }
+
+  get contextName(): string {
+    return 'payment/transaction'
+  }
+
+  get amount(): number {
+    return this._amount
+  }
+
+  get orderId(): string {
+    return this._orderId
+  }
+
+  get status(): string {
+    return this._status
+  }
+
+  validate(): void {
+    if (this._amount <= 0) {
+      this.addNotificationError('Amount must be greater than 0')
+    }
+
+    this.throwIfHasNotificationErrors()
+  }
+
+  private approve(): void {
+    this._status = TransactionStatus.Approved
+  }
+
+  private decline(): void {
+    this._status = TransactionStatus.Declined
+  }
+
+  process(): void {
+    if (this._status === TransactionStatus.Declined) {
+      this.addNotificationError('Transaction declined')
+    }
+
+    if (this._status === TransactionStatus.Approved) {
+      this.addNotificationError('Transaction already approved')
+    }
+
+    this.throwIfHasNotificationErrors()
+
+    if (this._amount >= 100) {
+      this.approve()
+    } else {
+      this.decline()
+    }
+  }
+}
