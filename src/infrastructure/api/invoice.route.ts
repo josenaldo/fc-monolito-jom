29dc9d4f9140f4807db2db80e0a267b00295092f
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2024-07-20 11:50:43 -0300

    feat: invoice api

---
+++ src/infrastructure/api/invoice.route.ts
@@ -0,0 +1,19 @@
+import { InvoiceFacadeFactory } from '@/modules/invoice/factory/invoice.facade.factory'
+import express from 'express'
+
+export const invoiceRoute = express.Router()
+
+invoiceRoute.get('/:id', async (req, res) => {
+  try {
+    const invoicefacade = InvoiceFacadeFactory.create()
+
+    const result = await invoicefacade.find({ id: req.params.id })
+
+    return res.status(200).send(result)
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).send({ error: error.message })
+    }
+    return res.status(500).send({ error: error })
+  }
+})
--- src/infrastructure/app-config.ts
+++ src/infrastructure/app-config.ts
@@ -1,5 +1,6 @@
 import { checkoutRoute } from '@/infrastructure/api/checkout.route'
 import { clientsRoute } from '@/infrastructure/api/clients.route'
+import { invoiceRoute } from '@/infrastructure/api/invoice.route'
 import { productsRoute } from '@/infrastructure/api/products.route'
 import express, { Express } from 'express'
 
@@ -8,3 +9,4 @@ app.use(express.json())
 app.use('/products', productsRoute)
 app.use('/clients', clientsRoute)
 app.use('/checkout', checkoutRoute)
+app.use('/invoices', invoiceRoute)
